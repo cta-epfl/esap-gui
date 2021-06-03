ee01ec50e69386edf0f8d0f1aa1ee4fa21b20d18
Nico Vermaas <vermaas@astron.nl>
2021-06-03 10:40:41 +0200

    storing logo as web accessable assets

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -56,7 +56,7 @@ export default function Routes() {
           </BasketContextProvider>
         </Route>
       </Switch>
-      <footer><small>esap-gui version 1 jun 2021</small></footer>
+      <footer><small>esap-gui version 3 jun 2021</small></footer>
     </Router>
   );
 }
