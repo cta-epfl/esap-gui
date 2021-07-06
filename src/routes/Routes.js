c84e7464c7873e1967320ece2ce91c4b86e842e4
Nico Vermaas <vermaas@astron.nl>
2021-07-06 08:39:28 +0200

    switching to esap-api on port 80 instead of port 5555

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -72,7 +72,7 @@ export default function Routes() {
 
         </Switch>
 
-      <footer><small>esap-gui version 2 jul 2021 - 15:00</small></footer>
+      <footer><small>esap-gui version 6 jul 2021 - 8:00</small></footer>
     </Router>
   );
 }
