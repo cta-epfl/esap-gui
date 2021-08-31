523ccc82658a4de665d4682ff61b87be33b6ba01
Nico Vermaas <vermaas@astron.nl>
2021-08-31 10:00:28 +0000

    Update Routes.js

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -87,7 +87,7 @@ export default function Routes() {
 
         </Switch>
 
-      <footer><small>esap-gui version 27 aug 2021 - 14:00</small></footer>
+      <footer><small>esap-gui version 31 aug 2021</small></footer>
     </Router>
   );
 }
