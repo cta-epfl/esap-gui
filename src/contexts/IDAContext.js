e52979a66fbceb32ecb8cd06c507588743afe6a4
stvoutsin <steliosvoutsinas@yahoo.com>
2021-10-07 16:38:34 +0300

    Redesigning UI for Interactive Analysis (two-step process)

--- src/contexts/IDAContext.js
+++ src/contexts/IDAContext.js
@@ -8,9 +8,9 @@ export function IDAContextProvider({ children }) {
 
     const { api_host } = useContext(GlobalContext);
     const [idaSystemURL, setIdaSystemURL] = useState();
-    const [jnotebookURL, setJnotebookURL] = useState();
+    const [workflowURL, setWorkflowURL] = useState();
     const [batchsystemsURL, setBatchsystemsURL] = useState();
-    const [list_of_jnotebooks, setList_of_jnotebooks] = useState();
+    const [list_of_workflows, setList_of_workflows] = useState();
     const [list_of_idaSystems, setList_of_idaSystems] = useState();
 
     // Fetch Notebooks
@@ -18,8 +18,8 @@ export function IDAContextProvider({ children }) {
       axios
       .get(api_host + "ida/workflows/search")
         .then((response) => {
-          setList_of_jnotebooks(response.data.results);
-          setJnotebookURL(response.data.results[0].url);
+          setList_of_workflows(response.data.results);
+          setWorkflowURL(response.data.results[0].url);
         });
     }, [api_host]);
 
@@ -40,12 +40,12 @@ export function IDAContextProvider({ children }) {
             value={{
                 idaSystemURL,
                 setIdaSystemURL,
-                jnotebookURL,
-                setJnotebookURL,
+                workflowURL,
+                setWorkflowURL,
                 batchsystemsURL,
                 setBatchsystemsURL,
-                list_of_jnotebooks,
-                setList_of_jnotebooks,
+                list_of_workflows,
+                setList_of_workflows,
                 list_of_idaSystems,
                 setList_of_idaSystems
             }}
