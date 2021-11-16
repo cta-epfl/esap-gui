c972c44c2e5adbf712448e65dba25df500ca7bca
stvoutsin <steliosvoutsinas@yahoo.com>
2021-11-16 14:43:32 +0200

    Added Loading message to Workflows search page / Move code from IDAContext to service

--- src/contexts/IDAContext.js
+++ src/contexts/IDAContext.js
@@ -13,27 +13,6 @@ export function IDAContextProvider({ children }) {
     const [list_of_workflows, setList_of_workflows] = useState();
     const [list_of_idaSystems, setList_of_idaSystems] = useState();
 
-    // Fetch Notebooks
-    useEffect(() => {
-      axios
-      .get(api_host + "ida/workflows/search")
-        .then((response) => {
-          setList_of_workflows(response.data.results);
-          setWorkflowURL(response.data.results[0].url);
-        });
-    }, [api_host]);
-
-
-    // Fetch JHubs
-    useEffect(() => {
-      axios
-      .get(api_host + "ida/facilities/search")
-        .then((response) => {
-          setList_of_idaSystems(response.data.results);
-          setIdaSystemURL(response.data.results[0].url);
-        });
-    }, [api_host]);
-
 
     return (
         <IDAContext.Provider
